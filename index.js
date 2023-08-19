cd2945e61de3c48421049f7a33ce5f4a569c12e4
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-20 02:02:02 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const input = document.getElementById("input");
+const sectionTwoCont = document.getElementById("section-two-container");
 
-document.getElementById("searchBtn").addEventListener("click", () => {
-  console.log(input.value);
+document.getElementById("searchBtn").addEventListener("click", async () => {
+  fetch(`https://www.omdbapi.com/?t=${input.value}&apikey=7b7f0630`)
+    .then((response) => response.json())
+    .then((obj) => {
+      console.log(obj);
+
+      sectionTwoCont.innerHTML += `
+
+              <div class="movie-info-container">
+                <div class="movie-img-cont">
+                  <img src="${obj.Poster}" class="movie-img" />
+                </div>
+                <div class="movie-info-box">
+                  <div class="child-one">
+                    <h4>${obj.Title}</h4>
+                    <img src="images/StarIcon.png" class="star-icon" />
+                    <h6>${obj.imdbRating}</h6>
+                  </div>
+                  <div class="child-two">
+                    <p>${obj.Runtime}</p>
+                    <p>${obj.Genre}</p>
+                    <div class="add-icon-container">
+                      <img src="images/plusicon.png" />
+                      <p>Watchlist</p>
+                    </div>
+                  </div>
+                  <div class="child-three">
+                    <p>
+                     ${obj.Plot}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              `;
+    });
 });
